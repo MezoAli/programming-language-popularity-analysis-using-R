e01b3ce5c92b46d928cad6ccf632647ae72c72d4
Moutaz Ali <moutaz.ali.dev@gmail.com>
2025-03-16 03:47:51 +0200

    what is the percentage of r questions asked in 2020

--- solution.R
+++ solution.R
@@ -8,3 +8,8 @@ library(dplyr)
 library(ggplot2)
 
 data <- read_csv("stack_overflow_data.csv")
+
+r_2020 <- data %>% 
+  filter(year == 2020) %>% 
+  mutate(percentage = (num_questions / sum(num_questions) * 100)) %>% 
+  filter(tag == "r")
